8227cd3d04169f0d388c9ddb66c4a8902cfd4d01
Ashrafpallat <ashrafpallat123@gmail.com>
2025-02-21 22:33:25 +0530

    timestsamp added in url model

--- models/urlModel.js
+++ models/urlModel.js
@@ -5,7 +5,9 @@ const urlSchema = new mongoose.Schema({
     originalUrl: { type: String, required: true },
     shortUrl: { type: String, required: true, unique: true },
     clicks: { type: Number, default: 0 }
-  });
+  },
+  { timestamps: true } 
+);
   
   const Url = mongoose.model("Url", urlSchema);
   export default Url;
