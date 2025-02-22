01286d93046a2b00ffcfe19a72c5eae61b9eb81f
Ashrafpallat <ashrafpallat123@gmail.com>
2025-02-22 19:59:38 +0530

    same site none

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
     res.status(200).json({ message: "Login successful", token, user });
